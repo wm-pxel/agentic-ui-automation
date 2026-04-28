1c8bf695aa1c69364b3d06c8caf38d5306b1ac29
Chris Maier <cmaier@westmonroe.com>
2026-04-28 13:11:38 -0500

    chore: scaffold TypeScript project

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./domain/schema.js";
+export * from "./domain/validation.js";
+export * from "./parsing/loadRecords.js";
+export * from "./audit/auditStore.js";
+export * from "./orchestrator/runWorkflow.js";
+export * from "./adapters/contract.js";
